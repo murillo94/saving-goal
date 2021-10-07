d5c3818c4674b774d1a5c07ba9384c8cc2a498c1
Murillo de Miranda Pereira <murillomir0@gmail.com>
2021-10-06 22:42:11 -0300

    feat: better style on body

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-import { getCssText } from 'theme/stitches.config';
+import { getCssText, theme } from 'theme/stitches.config';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
         <Head>
-          <style
-            id="stitches"
-            dangerouslySetInnerHTML={{ __html: getCssText() }}
-          />
           <link
             href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600&display=optional"
             rel="stylesheet"
@@ -18,6 +14,26 @@ export default class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600&display=optional"
             rel="stylesheet"
+          />
+          <style
+            id="stitches"
+            dangerouslySetInnerHTML={{ __html: getCssText() }}
+          />
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `
+                body {
+                  -webkit-font-smoothing: antialiased;
+	                -moz-osx-font-smoothing: grayscale;
+                  background-color: ${theme.colors.blueGray10};
+                  margin: 0;
+                }
+
+                *, :after, :before {
+                  box-sizing: border-box;
+                }
+              `,
+            }}
           />
         </Head>
         <body>
