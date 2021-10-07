6e058adb7cdfcc8c0a7ce1f25a0e0388f4bbe281
Murillo de Miranda Pereira <murillomir0@gmail.com>
2021-10-06 23:46:48 -0300

    feat: add unique id on ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
+import { IdProvider } from '@radix-ui/react-id';
 import Head from 'next/head';
 
 import type { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <>
+  <IdProvider>
     <Head>
       <title>Savings plan simulation</title>
       <meta name="description" content="Plan your saving goal easily" />
       <link rel="shortcut icon" type="image/x-icon" href="/favicon.png" />
     </Head>
     <Component {...pageProps} />
-  </>
+  </IdProvider>
 );
 
 export default App;
