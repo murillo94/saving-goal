6171344168a53651c1e9ce73da2037f7783347da
Murillo de Miranda Pereira <murillomir0@gmail.com>
2021-10-09 16:54:55 -0300

    feat: add options on format currency util

--- src/utils/__tests__/currency.test.ts
+++ src/utils/__tests__/currency.test.ts
@@ -12,4 +12,16 @@ describe('currency', () => {
 
     expect(month).toBe('1,000.00');
   });
+
+  it('should return currency with decimal scale', () => {
+    const month = formatCurrency('1000.3739', { decimalScale: 2 });
+
+    expect(month).toBe('1,000.37');
+  });
+
+  it('should return currency with prefix', () => {
+    const month = formatCurrency('1000.00', { prefix: '$' });
+
+    expect(month).toBe('$1,000.00');
+  });
 });
--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -1,7 +1,22 @@
 import { formatValue } from 'react-currency-input-field';
 
-export const formatCurrency = (value: string) =>
-  formatValue({ value, ...optionsCurrency });
+type FormatValueOptions = {
+  decimalSeparator?: string;
+  groupSeparator?: string;
+  disableGroupSeparators?: boolean;
+  decimalScale?: number;
+  prefix?: string;
+  suffix?: string;
+};
+
+export const formatCurrency = (
+  value: string,
+  options: FormatValueOptions = {},
+) => {
+  const customOptions = { ...optionsCurrency, ...options };
+
+  return formatValue({ value, ...customOptions });
+};
 
 export const optionsCurrency = {
   groupSeparator: ',',
